7d558501ee3d2984d071a52e7e61014fe788e49b
DESKTOP-I1TI6HD\shelt <sheltarywick@yahoo.com>
2020-12-09 14:58:39 +0300

    Скачать файл JP.zip.

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+import urllib.request
+print("Download JP.zip")
+print("Process...")
+destination = 'JP.zip'
+url = 'http://download.geonames.org/export/dump/JP.zip'
+urllib.request.urlretrieve(url, destination)
+print("Done.")
